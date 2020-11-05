02c8140bff4f831f6f6802837af14edcf37e95c0
Sang-Hyun  Lee <sang-hyunlee@Sang-Hyuns-MacBook-Air.local>
2020-11-05 11:25:30 +0900

    Complete Renenwal

--- src/EVM_Python/install.py
+++
@@ -1,6 +0,0 @@
-# Generated with SMOP  0.41
-from libsmop import *
-# install.m
-
-    addpath('./matlabPyrTools')
-    addpath('./matlabPyrTools/MEX')
--- src/EVM_Python/make.py
+++
@@ -1,8 +0,0 @@
-# Generated with SMOP  0.41
-from libsmop import *
-# make.m
-
-    
-    # Build matlabPyrTools
-    fprintf('Building matlabPyrTools...\n')
-    run(fullfile('matlabPyrTools','Mex','compilePyrTools.m'))
