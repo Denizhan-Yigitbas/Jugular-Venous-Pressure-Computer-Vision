8a827975c08531b84b02e5d7e09c3c256bd3db92
Sang-Hyun  Lee <sang-hyunlee@sang-hyuns-air.dyndns.rice.edu>
2021-03-22 21:51:21 -0500

    Restructured sticker detection to output min/max x and y coordinates

--- src/Cropping/sticker_detection.py
+++ src/Cropping/sticker_detection.py
@@ -1,64 +1,124 @@
 import cv2
 import numpy as np
 
-filename = '/Users/sang-hyunlee/Desktop/RotatedCirclescopy.jpeg'
-
-# Load image
-im = cv2.imread(filename, cv2.IMREAD_COLOR)
-
-# Erode the image (filtering)
-im_orig = im
-
-_, im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
-
-im = 255 - im
-im = 255 - cv2.erode(im, np.ones((3, 3)), iterations=8)
+file = '/Users/sang-hyunlee/Desktop/RotatedCirclescopy.jpeg'
 
 
-# Set our filtering parameters
-# Initialize parameter setting using cv2.SimpleBlobDetector
-params = cv2.SimpleBlobDetector_Params()
+def sticker_detection_coords(video_stack):
+    """
+        For an input frame of video, detects red stickers (circle/oval), finds the center x,y coordinates of each sticker in each
+        frame, and returns the min_x, min_y, max_x, max_y.
+    """
 
-# Set Area filtering parameters
-params.filterByArea = False
-params.maxArea = 1000
+    # Set our filtering parameters
+    # Initialize parameter setting using cv2.SimpleBlobDetector
+    params = cv2.SimpleBlobDetector_Params()
 
-# Set Circularity filtering parameters
-params.filterByCircularity = False
-params.minCircularity = 0.3
+    # Set Area filtering parameters
+    params.filterByArea = False
+    params.maxArea = 1000
 
-# Set Convexity filtering parameters
-params.filterByConvexity = True
-params.minConvexity = 0.2
+    # Set Circularity filtering parameters
+    params.filterByCircularity = False
+    params.minCircularity = 0.3
 
-# Set inertia filtering parameters
-params.filterByInertia = True
-params.minInertiaRatio = 0.05
+    # Set Convexity filtering parameters
+    params.filterByConvexity = True
+    params.minConvexity = 0.2
 
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector_create(params)
+    # Set inertia filtering parameters
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.05
 
-# Detect blobs
-keypoints = detector.detect(im)
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
 
-# Find the center points of each sticker & indicate with small black circles
-xycoord = []
+    # Initialize array for tuples of x,y coordinates
+    xycoord = []
 
-for idx in range(len(keypoints)):
-    xycoord.append((keypoints[idx].pt[0], keypoints[idx].pt[1]))
-    cv2.circle(im_orig, (int(keypoints[idx].pt[0]), int(keypoints[idx].pt[1])), 10, (0, 0, 0), 10)
+    # Iterate through each frame of video and find the x,y coordinates of
+    for i in range(len(video_stack)):
 
-# Draw blobs on our image as red circles
-blobs = cv2.drawKeypoints(im_orig, keypoints, np.array([]), (255, 0, 0),
-                          cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        frame = video_stack[i]
 
-"""
-number_of_blobs = len(keypoints)
-text = "Number of Circular Blobs: " + str(len(keypoints))
-cv2.putText(blobs, text, (20, 550),
-            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2)
-"""
-# Show blobs
-cv2.imshow("Filtering Red Stickers", blobs)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        im = frame.copy()
+
+        # Erosion to filter
+        _, im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
+
+        im = 255 - im
+        im = 255 - cv2.erode(im, np.ones((3, 3)), iterations=8)
+
+        # Detect blobs
+        keypoints = detector.detect(im)
+
+        # Find the center points of each sticker
+        for j in range(len(keypoints)):
+            xycoord.append((keypoints[j].pt[0], keypoints[j].pt[1]))
+
+    # Find min_x, min_y, max_x, max_y for cropping whole video
+    min_x = min(xycoord, key=lambda t: t[0])[0]
+    min_y = min(xycoord, key=lambda t: t[1])[1]
+    max_x = max(xycoord, key=lambda t: t[0])[0]
+    max_y = max(xycoord, key=lambda t: t[1])[1]
+
+    return min_x, min_y, max_x, max_y
+
+
+def sticker_detection_plot(filename):
+    # Load image
+    im = cv2.imread(filename, cv2.IMREAD_COLOR)
+
+    # Erode the image (filtering)
+    im_orig = im
+
+    _, im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
+
+    im = 255 - im
+    im = 255 - cv2.erode(im, np.ones((3, 3)), iterations=8)
+
+    # Set our filtering parameters
+    # Initialize parameter setting using cv2.SimpleBlobDetector
+    params = cv2.SimpleBlobDetector_Params()
+
+    # Set Area filtering parameters
+    params.filterByArea = False
+    params.maxArea = 1000
+
+    # Set Circularity filtering parameters
+    params.filterByCircularity = False
+    params.minCircularity = 0.3
+
+    # Set Convexity filtering parameters
+    params.filterByConvexity = True
+    params.minConvexity = 0.2
+
+    # Set inertia filtering parameters
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.05
+
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Detect blobs
+    keypoints = detector.detect(im)
+
+    # Find the center points of each sticker & indicate with small black circles
+    xycoord = []
+
+    for idx in range(len(keypoints)):
+        xycoord.append((keypoints[idx].pt[0], keypoints[idx].pt[1]))
+        cv2.circle(im_orig, (int(keypoints[idx].pt[0]), int(keypoints[idx].pt[1])), 10, (0, 0, 0), 10)
+
+    # Draw blobs on our image as red circles
+    blobs = cv2.drawKeypoints(im_orig, keypoints, np.array([]), (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Show blobs
+    cv2.imshow("Filtering Red Stickers", blobs)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return
+
+
+sticker_detection_plot(file)
