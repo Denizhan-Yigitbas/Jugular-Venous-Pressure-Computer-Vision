e1c689ecb2f7d0fd570759b5d08a6bc34171e4f5
Roy Phillips <ap60@rice.edu>
2021-03-27 18:30:07 -0500

    Distance function added to sticker_detection.py

--- src/Cropping/sticker_detection.py
+++ src/Cropping/sticker_detection.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+from itertools import combinations
 
-file = '/Users/sang-hyunlee/Desktop/RotatedCirclescopy.jpeg'
+
 
 
 def sticker_detection_coords(video_stack):
@@ -121,11 +122,11 @@ def sticker_detection_plot(filename):
     blobs = cv2.drawKeypoints(im_orig, keypoints, np.array([]), (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Show blobs
-    cv2.imshow("Filtering Red Stickers", blobs)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow("Filtering Red Stickers", blobs)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
-    return diameters
+    return np.array(diameters), np.array(xycoord)
 
 
 def pxl_to_dist(sticker_diameter, pixel_diameter):
@@ -137,9 +138,39 @@ def pxl_to_dist(sticker_diameter, pixel_diameter):
 
     return ratio
 
+def calc_distance(pxl_ratio, coords):
+    """
+    Given the ratio between distance and pixels, and a set of coordinates, returns distance between all coordinates.
+    :param pxl_ratio: output of pxl_to_dist, equivalent to diameter(inches)/diameter(pixels)
+    :param coords: list of sticker centers as (x,y) pixel values
+    :return: distances list of distances between points, list of coord pairs corresponding to those distances
+    """
+    distances = []
 
-#diameter = sticker_detection_plot(file)
-#print(pxl_to_dist(1, diameter[4]))
+    coord_combo = combinations(coords, 2)
+    coord_pairs = []
+    for coords in coord_combo:
+        [[x1, y1], [x2, y2]] = coords
+        distance = pxl_ratio*np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        distances.append(distance)
+        coord_pairs.append(coords)
+
+    return distances, coord_pairs
+
+
+if __name__ == "__main__":
+    root = '/Users/royphillips/Documents/Rice/elec494/S21/test_videos/'
+    file = root + 'red_dots_img.jpeg'
+    diameters, xycoord = sticker_detection_plot(file)
+
+    sticker_diam = 1
+    pxl_ratio = pxl_to_dist(sticker_diam, diameters[2])
+
+
+    distances, pair_combos = calc_distance(pxl_ratio,xycoord[:3,:])
+
+    print(distances)
+    print(pair_combos)
 
 # [263.0073547363281, 238.82835388183594, 371.4798278808594, 161.4182891845703, 155.4551239013672, 216.84242248535156]
 # [(1408.520751953125, 1496.6676025390625), (989.9877319335938, 1175.0413818359375), (923.7012329101562, 774.256103515625), (1518.4801025390625, 669.0260009765625), (192.65757751464844, 422.47564697265625), (1488.2816162109375, 107.33660125732422)]
