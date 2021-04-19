e86d3075eac4c94f6e78f2f470fb564afbb08d58
JoshKowal <endgamekowal@gmail.com>
2021-04-18 22:12:56 -0500

    Updated test_click.py

--- src/distance_scale/test_click.py
+++ src/distance_scale/test_click.py
@@ -21,9 +21,10 @@ class LineBuilder:
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
         if len(self.xs) == 2:
+            # We are in the case where the line is drawn
             self.distance = math.sqrt((self.xs[0] - self.xs[1]) ** 2 + (self.ys[0] - self.ys[1]) ** 2)
             ax.annotate(f'Line distance is {self.distance} pixels', xy=(260, 20), xycoords='figure pixels')
-            plt.savefig('testimage.png')
+            # plt.savefig('testimage.png')
             ax.set_title('Click anywhere on the image to exit')
         if len(self.xs) == 3:
             self.xs.pop()
@@ -40,6 +41,8 @@ linebuilder = LineBuilder(line)
 plt.imshow(im)
 plt.show()
 
-x, y = linebuilder.xs, linebuilder.ys
-print(f"The first point's coordinates are: ({x[0]}, {y[0]})")
-print(f"The second point's coordinates are: ({x[1]}, {y[1]})")
+x, y, d = linebuilder.xs, linebuilder.ys, linebuilder.distance
+print("\n")
+print(f"The first point's coordinates are: ({round(x[0],2)}, {round(y[0],2)}).")
+print(f"The second point's coordinates are: ({round(x[1],2)}, {round(y[1],2)}).")
+print(f"The distance of the line is {round(d,2)} pixels.")
