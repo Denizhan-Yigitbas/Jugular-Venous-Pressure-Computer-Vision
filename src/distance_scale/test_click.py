16f45b36a302166169b8b2bfa690863f1c81ed7d
JoshKowal <endgamekowal@gmail.com>
2021-04-18 22:44:13 -0500

    Updated test_click.py

--- src/distance_scale/test_click.py
+++ src/distance_scale/test_click.py
@@ -20,14 +20,11 @@ class LineBuilder:
         self.ys.append(event.ydata)
         self.line.set_data(self.xs, self.ys)
         self.line.figure.canvas.draw()
-        if len(self.xs) == 1:
-            ax.set_title('Click on the Circle Close to the Sternum')
         if len(self.xs) == 2:
-            # We are in the case where the line is drawn
             self.distance = math.sqrt((self.xs[0] - self.xs[1]) ** 2 + (self.ys[0] - self.ys[1]) ** 2)
             ax.annotate(f'Line distance is {self.distance} pixels', xy=(260, 20), xycoords='figure pixels')
             # plt.savefig('testimage.png')
-            ax.set_title('Great! Click Anywhere on the Image to Exit!')
+            ax.set_title('Click anywhere on the image to exit')
         if len(self.xs) == 3:
             self.xs.pop()
             self.ys.pop()
@@ -36,15 +33,13 @@ class LineBuilder:
 
 im = image.imread('/Users/joshuakowal/Downloads/bball1.jpeg')
 fig, ax = plt.subplots()
-ax.set_title('Click at the Top of the JVP')
+ax.set_title('click to build line segments')
 line, = ax.plot([0], [0])  # empty line
 linebuilder = LineBuilder(line)
 
 plt.imshow(im)
 plt.show()
 
-x, y, d = linebuilder.xs, linebuilder.ys, linebuilder.distance
-print("\n")
-print(f"The first point's coordinates are: ({round(x[0],2)}, {round(y[0],2)}).")
-print(f"The second point's coordinates are: ({round(x[1],2)}, {round(y[1],2)}).")
-print(f"The distance of the line is {round(d,2)} pixels.")
+x, y = linebuilder.xs, linebuilder.ys
+print(f"The first point's coordinates are: ({x[0]}, {y[0]})")
+print(f"The second point's coordinates are: ({x[1]}, {y[1]})")
